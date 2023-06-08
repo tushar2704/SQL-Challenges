1f87824df68de23bda58dfa648c4ce669764ed1f
tushar2704 <tushar.inseec@gmail.com>
2023-06-08 22:30:24 +0530

    update

--- Case Study #1 Danny's Diner/#1Dannys_Diner.sql
+++ Case Study #1 Danny's Diner/#1Dannys_Diner.sql
@@ -20,3 +20,37 @@ CREATE TABLE members (
 	"customer_id" VARCHAR(1),
 	"join_date" DATE
 	);
+
+
+--Inserting Values in Tables
+INSERT INTO sales
+	("customer_id", "order_date","product_id")
+VALUES
+	('A', '2021-01-01', '1'),
+  	('A', '2021-01-01', '2'),
+  	('A', '2021-01-07', '2'),
+  	('A', '2021-01-10', '3'),
+  	('A', '2021-01-11', '3'),
+  	('A', '2021-01-11', '3'),
+  	('B', '2021-01-01', '2'),
+  	('B', '2021-01-02', '2'),
+  	('B', '2021-01-04', '1'),
+  	('B', '2021-01-11', '1'),
+  	('B', '2021-01-16', '3'),
+  	('B', '2021-02-01', '3'),
+  	('C', '2021-01-01', '3'),
+  	('C', '2021-01-01', '3'),
+  	('C', '2021-01-07', '3');
+	
+INSERT INTO menu
+	("product_id", "product_name", "price")
+VALUES
+	('1','sushi','10'),
+	('2','curry','15'),
+	('3','ramen','12');
+	
+INSERT INTO members
+	("customer_id", "join_date")
+VALUES
+	('A','2021-01-07'),
+	('B','2021-01-09');
