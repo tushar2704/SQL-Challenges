6adee4800828b955ab11e5e9fb081ece7dbc0d21
tushar2704 <tushar.inseec@gmail.com>
2023-06-09 02:07:29 +0530

    update

--- Case Study #1 Danny's Diner/#1Dannys_Diner_Query.sql
+++ Case Study #1 Danny's Diner/#1Dannys_Diner_Query.sql
@@ -23,5 +23,34 @@ SELECT
 FROM dannys_diner.sales
 GROUP BY customer_id;
 
+-- 3. What was the first item from the menu purchased by each customer?
+WITH ordered_sale AS (
+	SELECT
+		sales.customer_id,
+		sales.order_date,
+		menu.product_name,
+		DENSE_RANK() OVER(
+			PARTITION BY sales.customer_id
+			ORDER BY sales.order_date) AS rank
+	FROM dannys_diner.sales
+    JOIN dannys_diner.menu
+		ON sales.product_id=menu.product_id)
 
-	
+SELECT 
+	customer_id,
+	product_name
+FROM ordered_sale
+WHERE rank=1
+GROUP BY customer_id, product_name;
+
+
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+SELECT
+	menu.product_name,
+	COUNT(sales.product_id) AS most_purchased_item
+FROM dannys_diner.sales
+JOIN dannys_diner.menu
+	ON sales.product_id=menu.product_id
+GROUP BY menu.product_name
+ORDER BY most_purchased_item DESC
+LIMIT 1;
